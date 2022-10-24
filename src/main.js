9413950cb5e652a41410456a5f23b4f4d175a7a4
FrankCostta <fransta8@gmail.com>
2022-10-23 21:02:12 -0300

    atualizando

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const registrar = document.getElementById("registrar");
+
+/* Abre um modal com formulario de cadastro */
+registrar.addEventListener("click", ()=> {
+	document.getElementById("modal-registro").style.display = "block";
+});
